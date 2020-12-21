a3e4a0517e6479f504cacbf02de5a132ed50f796
danjones457 <danjonesy@ymail.com>
2020-12-21 02:08:11 +0000

    Fixed overflow of body. Changed class to classname

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
     <div className="App">
       <header>
         <ul>
-          <li onClick={() => setPage(1)} className="text-green"><i class="far fa-user-circle"></i> <span className="hidden md:inline-block">About</span></li>
-          <li onClick={() => setPage(2)} className="text-green"><i class="fas fa-phone-alt"></i> <span className="hidden md:inline-block">Contact</span></li>
-          <li onClick={() => setPage(3)} className="text-green"><i class="fas fa-briefcase"></i> <span className="hidden md:inline-block">Experience</span></li>
-          <li onClick={() => setPage(4)} className="text-green"><i class="fas fa-graduation-cap"></i> <span className="hidden md:inline-block">Education</span></li>
-          <li onClick={() => setPage(5)} className="text-green"><i class="fas fa-lightbulb"></i> <span className="hidden md:inline-block">Projects</span></li>
+          <li onClick={() => setPage(1)} className="text-green"><i className="far fa-user-circle"></i> <span className="hidden md:inline-block">About</span></li>
+          <li onClick={() => setPage(2)} className="text-green"><i className="fas fa-phone-alt"></i> <span className="hidden md:inline-block">Contact</span></li>
+          <li onClick={() => setPage(3)} className="text-green"><i className="fas fa-briefcase"></i> <span className="hidden md:inline-block">Experience</span></li>
+          <li onClick={() => setPage(4)} className="text-green"><i className="fas fa-graduation-cap"></i> <span className="hidden md:inline-block">Education</span></li>
+          <li onClick={() => setPage(5)} className="text-green"><i className="fas fa-lightbulb"></i> <span className="hidden md:inline-block">Projects</span></li>
         </ul>
       </header>
-      <div className="w-full p-5">
+      <div className="w-full p-5 overflow-y-auto">
         <div className="typewriter w-min m-auto text-center">
           <h1 className="text-green font-bold text-3xl">Dan Jones</h1>
         </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -27,39 +27,39 @@ class About extends React.Component {
                 <p className="text-gray-400 text-xl">Languages</p>
             </div>
             <div className="grid grid-cols-2 md:grid-cols-6 text-l text-center text-gray-400">
-                <div className="py-2"><i class="fab fa-php text-3xl text-green"></i> <p>PHP</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>TypeScript</p></div>
-                <div className="py-2"><i class="fab fa-js text-3xl text-green"></i> <p>JavaScript</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>Dart</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>C#</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>SQL</p></div>
-                <div className="py-2"><i class="fab fa-html5 text-3xl text-green"></i> <p>HTML</p></div>
-                <div className="py-2"><i class="fab fa-css3-alt text-3xl text-green"></i> <p>CSS</p></div>
-                <div className="py-2"><i class="fab fa-sass text-3xl text-green"></i> <p>SASS</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>XML</p></div>
-                <div className="py-2"><i class="fab fa-java text-3xl text-green"></i> <p>Java</p></div>
+                <div className="py-2"><i className="fab fa-php text-3xl text-green"></i> <p>PHP</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>TypeScript</p></div>
+                <div className="py-2"><i className="fab fa-js text-3xl text-green"></i> <p>JavaScript</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>Dart</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>C#</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>SQL</p></div>
+                <div className="py-2"><i className="fab fa-html5 text-3xl text-green"></i> <p>HTML</p></div>
+                <div className="py-2"><i className="fab fa-css3-alt text-3xl text-green"></i> <p>CSS</p></div>
+                <div className="py-2"><i className="fab fa-sass text-3xl text-green"></i> <p>SASS</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>XML</p></div>
+                <div className="py-2"><i className="fab fa-java text-3xl text-green"></i> <p>Java</p></div>
             </div>  
             <div className="pt-2">
                 <p className="text-gray-400 text-xl">Frameworks</p>
             </div>
             <div className="grid grid-cols-2 md:grid-cols-6 text-l text-center text-gray-400">
-                <div className="py-2"><i class="fab fa-laravel text-3xl text-green"></i> <p>Laravel</p></div>
-                <div className="py-2"><i class="fab fa-react text-3xl text-green"></i> <p>React</p></div>
-                <div className="py-2"><i class="fab fa-react text-3xl text-green"></i> <p>React Native</p></div>
-                <div className="py-2"><i class="fab fa-google text-3xl text-green"></i> <p>Flutter</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>Gradle</p></div>
-                <div className="py-2"><i class="fab fa-angular text-3xl text-green"></i> <p>AngularJS</p></div>
-                <div className="py-2"><i class="fab fa-java text-3xl text-green"></i> <p>Spring Boot</p></div>
+                <div className="py-2"><i className="fab fa-laravel text-3xl text-green"></i> <p>Laravel</p></div>
+                <div className="py-2"><i className="fab fa-react text-3xl text-green"></i> <p>React</p></div>
+                <div className="py-2"><i className="fab fa-react text-3xl text-green"></i> <p>React Native</p></div>
+                <div className="py-2"><i className="fab fa-google text-3xl text-green"></i> <p>Flutter</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>Gradle</p></div>
+                <div className="py-2"><i className="fab fa-angular text-3xl text-green"></i> <p>AngularJS</p></div>
+                <div className="py-2"><i className="fab fa-java text-3xl text-green"></i> <p>Spring Boot</p></div>
             </div>
             <div className="pt-2">
                 <p className="text-gray-400 text-xl">Testing frameworks</p>
             </div>
             <div className="grid grid-cols-2 md:grid-cols-6 text-l text-center text-gray-400">
-                <div className="py-2"><i class="fab fa-php text-3xl text-green"></i> <p>PHPUnit</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>JUnit</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>NUnit</p></div>
-                <div className="py-2"><i class="fab fa-laravel text-3xl text-green"></i> <p>Dusk</p></div>
-                <div className="py-2"><i class="fas fa-code text-3xl text-green"></i> <p>XUnit</p></div>
+                <div className="py-2"><i className="fab fa-php text-3xl text-green"></i> <p>PHPUnit</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>JUnit</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>NUnit</p></div>
+                <div className="py-2"><i className="fab fa-laravel text-3xl text-green"></i> <p>Dusk</p></div>
+                <div className="py-2"><i className="fas fa-code text-3xl text-green"></i> <p>XUnit</p></div>
             </div>
         </div>;
     }
