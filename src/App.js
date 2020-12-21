fe5f43ee52bdb6037d0a7ec619d641a9abdcfce4
danjones457 <danjonesy@ymail.com>
2020-12-21 12:47:06 +0000

    Made header monospace font. Moved css to use tailwind classes

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
   const [page, setPage] = useState(1);
 
   return (
-    <div className="App">
-      <header>
-        <ul>
-          <li onClick={() => setPage(1)} className="text-green"><i className="far fa-user-circle"></i> <span className="hidden md:inline-block">About</span></li>
-          <li onClick={() => setPage(2)} className="text-green"><i className="fas fa-phone-alt"></i> <span className="hidden md:inline-block">Contact</span></li>
-          <li onClick={() => setPage(3)} className="text-green"><i className="fas fa-briefcase"></i> <span className="hidden md:inline-block">Experience</span></li>
-          <li onClick={() => setPage(4)} className="text-green"><i className="fas fa-graduation-cap"></i> <span className="hidden md:inline-block">Education</span></li>
-          <li onClick={() => setPage(5)} className="text-green"><i className="fas fa-lightbulb"></i> <span className="hidden md:inline-block">Projects</span></li>
+    <div className="text-center flex h-full">
+      <header className="w-7 md:w-56 text-center">
+        <ul className="text-left p-0 absolute w-44 top-1/2 transform -translate-y-1/2 list-none left-2">
+          <li onClick={() => setPage(1)} className="text-green text-2xl pb-1 hover:animate-bounce-nav"><i className="far fa-user-circle w-1/6"></i> <span className="hidden md:inline-block">About</span></li>
+          <li onClick={() => setPage(2)} className="text-green text-2xl pb-1 hover:animate-bounce-nav"><i className="fas fa-phone-alt w-1/6"></i> <span className="hidden md:inline-block">Contact</span></li>
+          <li onClick={() => setPage(3)} className="text-green text-2xl pb-1 hover:animate-bounce-nav"><i className="fas fa-briefcase w-1/6"></i> <span className="hidden md:inline-block">Experience</span></li>
+          <li onClick={() => setPage(4)} className="text-green text-2xl pb-1 hover:animate-bounce-nav"><i className="fas fa-graduation-cap w-1/6"></i> <span className="hidden md:inline-block">Education</span></li>
+          <li onClick={() => setPage(5)} className="text-green text-2xl hover:animate-bounce-nav"><i className="fas fa-lightbulb w-1/6"></i> <span className="hidden md:inline-block">Projects</span></li>
         </ul>
       </header>
       <div className="w-full p-5 overflow-y-auto">
         <div className="typewriter w-min m-auto text-center">
-          <h1 className="text-green font-bold text-3xl">Dan Jones</h1>
+          <h1 className="text-green font-bold text-3xl overflow-hidden whitespace-nowrap tracking-widest border-r-4 border-solid border-green">Dan Jones</h1>
         </div>
         <BodyPage page={page} />
       </div>
