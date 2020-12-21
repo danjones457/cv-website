46d893cdfd8615a9ac791b94a1e0604adb4c1a02
danjones457 <danjonesy@ymail.com>
2020-12-21 16:56:50 +0000

    Moved all of timeline item to content

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,102 +1,52 @@
 import React from 'react';
 import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
-import Paper from '@material-ui/core/Paper';
 import Content from './Experience/Content';
 
 function Experience() {
-  return (
-            <div className="text-left">
-                <div>
-                    <p className="text-white text-4xl pb-2">Experience</p>
-                </div>
-                <Timeline align="alternate">
-                    <TimelineItem>
-                        <TimelineOppositeContent>
-                            <p className="text-white text-sm">
-                                January 2021 - Present
-                            </p>
-                        </TimelineOppositeContent>
-                        <TimelineSeparator>
-                            <TimelineDot className="border-green" variant="outlined" />
-                            <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent>
-                            <Content 
-                                logoUrl="o&d.webp" 
-                                companyName="Office & Dragons" 
-                                role="Software Engineer"
-                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in enim laoreet, sollicitudin nibh vehicula, pretium ipsum. Donec feugiat elit nec justo efficitur aliquet. Proin eget pulvinar neque. Maecenas non dapibus arcu. Fusce mattis nisl quam, ut auctor elit rhoncus vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec tellus velit, hendrerit sed nisl sit amet, tincidunt ornare velit. Sed suscipit imperdiet magna et eleifend."
-                            />
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineOppositeContent>
-                            <p className="text-white text-sm">
-                                August 2019 - December 2020
-                            </p>
-                        </TimelineOppositeContent>
-                        <TimelineSeparator>
-                            <TimelineDot className="border-green" variant="outlined" />
-                            <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent>
-                            <Content 
-                                logoUrl="netcompany.jpg" 
-                                companyName="Netcompany" 
-                                role="Software Development Consultant"
-                                description="Liasing with the project manager to give updates on team progression, preparing releases, and leading the team in the PM’s absence.
-                                Setup the first official football club in the UK"
-                            />
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineOppositeContent>
-                            <p className="text-white text-sm">
-                                September 2018 - April 2019
-                            </p>
-                        </TimelineOppositeContent>
-                        <TimelineSeparator>
-                            <TimelineDot className="border-green" variant="outlined" />
-                            <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent>
-                            <Content 
-                                logoUrl="cashcalc.png" 
-                                companyName="CashCalc" 
-                                role="Software Engineer (Part time)"
-                                description="Alongside my studies at university, I worked for CashCalc developing and maintaining their site.
-                                Building new features and bug fixing
-                                Automated testing (Both front & back end)"
-                            />
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineOppositeContent>
-                            <p className="text-white text-sm">
-                                June 2018 - September 2018
-                            </p>
-                        </TimelineOppositeContent>
-                        <TimelineSeparator>
-                            <TimelineDot className="border-green" variant="outlined" />
-                        </TimelineSeparator>
-                        <TimelineContent>
-                            <Content 
-                                logoUrl="cashcalc.png" 
-                                companyName="CashCalc" 
-                                role="Software Engineer Intern"
-                                description="I began as an intern at CashCalc and was then offered part-time work to complete during my third year of University"
-                            />
-                        </TimelineContent>
-                    </TimelineItem>
-                </Timeline>
+    return (
+        <div className="text-left">
+            <div>
+                <p className="text-white text-4xl pb-2">Experience</p>
             </div>
-        );
+            <Timeline align="alternate">
+                <Content 
+                    last={false}
+                    date="January 2021 - Present"
+                    logoUrl="o&d.webp" 
+                    companyName="Office & Dragons" 
+                    role="Software Engineer"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in enim laoreet, sollicitudin nibh vehicula, pretium ipsum. Donec feugiat elit nec justo efficitur aliquet. Proin eget pulvinar neque. Maecenas non dapibus arcu. Fusce mattis nisl quam, ut auctor elit rhoncus vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec tellus velit, hendrerit sed nisl sit amet, tincidunt ornare velit. Sed suscipit imperdiet magna et eleifend."
+                />
+                <Content 
+                    last={false}
+                    date="August 2019 - December 2020"
+                    logoUrl="netcompany.jpg" 
+                    companyName="Netcompany" 
+                    role="Software Development Consultant"
+                    description="Liasing with the project manager to give updates on team progression, preparing releases, and leading the team in the PM’s absence.
+                    Setup the first official football club in the UK"
+                />
+                <Content 
+                    last={false}
+                    date="September 2018 - April 2019"
+                    logoUrl="cashcalc.png" 
+                    companyName="CashCalc" 
+                    role="Software Engineer (Part time)"
+                    description="Alongside my studies at university, I worked for CashCalc developing and maintaining their site.
+                    Building new features and bug fixing
+                    Automated testing (Both front & back end)"
+                />
+                <Content 
+                    last={false}
+                    date="June 2018 - September 2018"
+                    logoUrl="cashcalc.png" 
+                    companyName="CashCalc" 
+                    role="Software Engineer Intern"
+                    description="I began as an intern at CashCalc and was then offered part-time work to complete during my third year of University"
+                />
+            </Timeline>
+        </div>
+    );
 }
 
 export default Experience;
--- src/pages/Experience/Content.js
+++ src/pages/Experience/Content.js
@@ -1,18 +1,37 @@
 import Paper from '@material-ui/core/Paper';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
 
 export default function Content(props) {
     return (
-        <Paper elevation={3} className="lg:flex p-2">
-            <div className="w-3/4 lg:w-1/4 pr-2 lg:m-auto">
-                <img src={process.env.PUBLIC_URL + '/work-logos/' + props.logoUrl} />
-            </div>
-            <div className="w-full lg:w-3/4">
-                <h1 className="text-2xl">
-                    {props.companyName}
-                </h1>
-                <p className="text-gray-500">{props.role}</p>
-                <p>{props.description}</p>
-            </div>
-        </Paper>
+        <TimelineItem>
+            <TimelineOppositeContent>
+                <p className="text-white text-sm">
+                    {props.date}
+                </p>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+                <TimelineDot className="border-green" variant="outlined" />
+                {!props.last ? <TimelineConnector /> : null}
+            </TimelineSeparator>
+            <TimelineContent>
+                <Paper elevation={3} className="lg:flex p-2">
+                    <div className="w-3/4 lg:w-1/4 pr-2 lg:m-auto">
+                        <img src={process.env.PUBLIC_URL + '/work-logos/' + props.logoUrl} />
+                    </div>
+                    <div className="w-full lg:w-3/4">
+                        <h1 className="text-2xl">
+                            {props.companyName}
+                        </h1>
+                        <p className="text-gray-500">{props.role}</p>
+                        <p>{props.description}</p>
+                    </div>
+                </Paper>
+            </TimelineContent>
+        </TimelineItem>
     );
 }
