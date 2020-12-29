53914bc784a2d8d2a322dfd4e9a084322a05852e
danjones457 <danjonesy@ymail.com>
2020-12-29 18:28:15 +0000

    Contact form styling. Clear form on submit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,21 +8,23 @@ export default function Contact() {
 
     emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, e.target, process.env.REACT_APP_EMAILJS_USER_ID)
       .then((result) => {
-          console.log(result.text);
+        document.getElementById("contactForm").reset();
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="">
+      <form id="contactForm" className="bg-gray-400 rounded-lg p-5 w-4/5 md:w-1/2 absolute top-1/2 transform -translate-y-1/2 float-left text-left" onSubmit={sendEmail}>
+        <label className="block text-gray-700 pb-1">Name</label>
+        <input type="text" name="user_name" className="rounded-md p-1 mb-2 w-4/5" />
+        <label className="block text-gray-700 pb-1">Email</label>
+        <input type="email" name="user_email" className="rounded-md p-1 mb-2 w-4/5" />
+        <label className="block text-gray-700 pb-1">Message</label>
+        <textarea name="message" className="rounded-md p-1 mb-2 w-4/5" />
+        <input type="submit" value="Send" className="block" />
+      </form>
+    </div>
   );
 }
