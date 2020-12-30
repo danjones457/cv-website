67ce2e643f1d6667abdb7b5e54f38815e3c1fd25
danjones457 <danjonesy@ymail.com>
2020-12-30 02:24:41 +0000

    Styling changes for contact page. Added subject to the form. Change to gitignore

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,15 +15,27 @@ export default function Contact() {
   }
 
   return (
-    <div className="">
-      <form id="contactForm" className="bg-gray-400 rounded-lg p-5 w-4/5 md:w-1/2 absolute top-1/2 transform -translate-y-1/2 float-left text-left" onSubmit={sendEmail}>
-        <label className="block text-gray-700 pb-1">Name</label>
-        <input type="text" name="user_name" className="rounded-md p-1 mb-2 w-4/5" />
-        <label className="block text-gray-700 pb-1">Email</label>
-        <input type="email" name="user_email" className="rounded-md p-1 mb-2 w-4/5" />
-        <label className="block text-gray-700 pb-1">Message</label>
-        <textarea name="message" className="rounded-md p-1 mb-2 w-4/5" />
-        <input type="submit" value="Send" className="block" />
+    <div className="text-left">
+      <div className="pb-5">
+        <h1 className="text-white text-4xl pb-4">Contact me</h1>
+        <p className="text-green">Have questions or comments?</p>
+      </div>
+      <form id="contactForm" className="w-4/5 md:w-1/2 text-left" onSubmit={sendEmail}>
+        <div className="float-left w-1/3 pr-2">
+          <label className="block text-green pb-1">Name</label>
+          <input type="text" name="user_name" className="rounded-md p-1 mb-2 w-full" />
+        </div>
+        <div className="float-left w-1/3 pr-2">
+          <label className="block text-green pb-1">Email</label>
+          <input type="email" name="user_email" className="rounded-md p-1 mb-2 w-full" />
+        </div>
+        <div className="float-left w-1/3">
+          <label className="block text-green pb-1">Subject</label>
+          <input type="Subject" name="subject" className="rounded-md p-1 mb-2 w-full" />
+        </div>
+        <label className="block text-green pb-1">Message</label>
+        <textarea name="message" className="rounded-md p-1 mb-2 w-full h-64" />
+        <input type="submit" value="Send" className="w-full rounded-md p-2 font-bold text-green bg-gray-900" />
       </form>
     </div>
   );
